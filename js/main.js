2910f1dd9734d49103e1f75849d275b6831279d2
sangjun <goahsj1@naver.com>
2024-07-29 17:45:21 +0900

    second commit

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(function(){
         },1000)
     })
     $(window).scroll(function(){
-        if($(this).scrollTop() >= 200){
+        if($(this).scrollTop() >= 200 && $(this).scrollTop() <= 4200){
             $('.quicksection').addClass('on')
             $('header').addClass('on')
         }
@@ -47,9 +47,27 @@ $(function(){
         $('.listbox > *').hide()
         $('.listbox > *').eq(idx).show()
     })
+    //공유탭머뉴
+
+    $('.sharetab li').click(function(e){
+        e.preventDefault()
+        $('.sharetab li').removeClass('on')
+        $(this).addClass('on')
+
+        let shareidx = $(this).index()
+        $('.sharesection > *').hide()
+        $('.swiper-button-next').show()
+        $('.swiper-button-prev').show()
+        $('.sharesection > *').eq(shareidx).show()
+    })
     // footer 버튼
     $('.bottomright .button').click(function(){
         $('.button .buttonlist').toggleClass('on')
         $('.changebutton').toggleClass('on')
     })
 })
+
+
+// window.addEventListener('scroll', function(){
+//     console.log(window.scrollY)
+// })
